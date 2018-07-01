db3c8dbc9ebe13a0975db0cf1759fef83e9c5317
jpatel0 <jaypatel9670@gmail.com>
2018-07-01 12:21:33 +0530

    1.4 Added Content Provider(data/PetProvider)

--- app/src/main/java/com/example/android/pets/data/PetContract.java
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -1,13 +1,22 @@
 package com.example.android.pets.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
+import java.net.URI;
+
 public final class PetContract {
+
+    public static final String CONTENT_AUTHORITY="com.example.android.pets";
+    public static final Uri BASE_CONTENT_URI=Uri.parse("content://"+CONTENT_AUTHORITY);
+    public static final String PATH_PET="pets";
 
     public PetContract() {
     }
 
     public static final class PetEntry implements BaseColumns{
+
+        public static final Uri PET_URI=Uri.withAppendedPath(BASE_CONTENT_URI,PATH_PET);
 
         public final static String TABLE_NAME = "pets";
         public final static String _ID = BaseColumns._ID;
